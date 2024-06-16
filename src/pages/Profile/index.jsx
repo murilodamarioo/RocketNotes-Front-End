9747e4f2c2739eace4c50b4bb3b65cd795956d41
Murilo Damario <murilo.gcdamario@gmail.com>
2024-06-16 17:56:34 -0300

    Implement update user avatar

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -4,8 +4,12 @@ import { Input } from '../../components/Input'
 import { Button } from '../../components/Button'
 import { Link } from 'react-router-dom'
 
+import { api } from '../../services/api'
+
 import { useState } from 'react'
 import { useAuth } from '../../hooks/auth'
+
+import avatarPlaceholder from '../../assets/avatar_placeholder.svg'
 
 export function Profile() {
 
@@ -16,6 +20,10 @@ export function Profile() {
     const [passwordOld, setPasswordOld] = useState('')
     const [passwordNew, setPasswordNew] = useState('')
 
+    const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder
+    const [avatar, setAvatar] = useState(avatarUrl)
+    const [avatarFile, setAvatarFile] = useState(null)
+
     async function handleUpdate() {
         const user = {
             name,
@@ -24,7 +32,15 @@ export function Profile() {
             old_password: passwordOld
         }
 
-        await updateProfile({user})
+        await updateProfile({user, avatarFile})
+    }
+
+    function handleChangeAvatar(event) {
+        const file = event.target.files[0]
+        setAvatarFile(file)
+
+        const imagePreview = URL.createObjectURL(file)
+        setAvatar(imagePreview)
     }
 
     return (
@@ -38,7 +54,7 @@ export function Profile() {
             <Form>
                 <Avatar>
                     <img 
-                        src="https://github.com/murilodamarioo.png" 
+                        src={avatar}
                         alt="Foto do usuário" 
                     />
 
@@ -47,6 +63,7 @@ export function Profile() {
                         <input 
                             id='avatar'
                             type='file'
+                            onChange={handleChangeAvatar}
                         />
                     </label>
                 </Avatar>
