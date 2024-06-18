aead8da7c92108e99a8812b629d367c37ff9ebcb
Murilo Damario <murilo.gcdamario@gmail.com>
2024-06-17 22:19:05 -0300

    implement handleRemove

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -6,45 +6,91 @@ import { Button } from '../../components/Button'
 import { Section } from '../../components/Section'
 import { ButtonText } from '../../components/ButtonText'
 
+import { useParams, useNavigate } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import { api } from '../../services/api'
+
 export function Details() {
+  const [data, setData] = useState(null)
+
+  const params = useParams()
+  const navigate = useNavigate()
+
+  function handleBack() {
+    navigate(-1)
+  }
+
+  async function handleRemove() {
+    const confirm = window.confirm('Tem certeza que deseja excluir essa nota?')
+
+    if (confirm) {
+      await api.delete(`/notes/${params.id}`)
+      navigate(-1)
+    }
+  }
+
+  useEffect(() => {
+    async function fetchNote() {
+      const response = await api.get(`/notes/${params.id}`)
+      setData(response.data)
+    }
+
+    fetchNote()
+  }, [])
+
   return (
     <Container>
       <Header />
+      {
+        data &&
+        <main>
+          <Content>
+            <ButtonText 
+              onClick={handleRemove}
+              title="Excluir nota" 
+            />
 
-      <main>
-        <Content>
-          <ButtonText title="Excluir nota" />
+            <h1>
+              {data.title}
+            </h1>
 
-          <h1>
-            Introdução ao React
-          </h1>
-
-          <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-            when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap 
-            into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum 
-            passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-          </p>
-
-          <Section title="Links úteis">
-            <Links>
-              <li>
-                <a href="#">https://rocketseat.com.br</a>
-              </li>
-              <li>
-                <a href="#">https://rocketseat.com.br</a>
-              </li>
-            </Links>
-          </Section>
-
-          <Section title="Marcadores">
-            <Tag title="express" />
-            <Tag title="node" />
-          </Section>
-
-          <Button title="Voltar" />
-        </Content>
-      </main>
+            <p>
+              {data.description}
+            </p>
+            {
+              data.links &&
+              <Section title="Links úteis">
+                <Links>
+                  {
+                    data.links.map(link => (
+                      <li key={String(link.id)}>
+                        <a href={link.url} target='_blank'>{link.url}</a>
+                      </li>
+                    ))
+                  }
+                </Links>
+              </Section>
+            }
+            {
+              data.tags &&
+              <Section title="Marcadores">
+                {
+                  data.tags.map(tag => (
+                    <Tag 
+                      key={String(tag.id)}
+                      title={tag.name} 
+                    />
+                  ))
+                }
+              </Section>
+            }
+            <Button 
+              onClick={handleBack}
+              title="Voltar" 
+            />
+          </Content>
+        </main>
+      }
     </Container>
   )
 }
